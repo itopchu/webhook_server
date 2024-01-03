0cda36c7b62f93cefb9390db263745db865ef6a8
gokkoylu <gokberk.koylu@yandex.com>
2024-01-03 12:19:33 +0100

    webhook created post part

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server); // Setup socket.io
+
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Webhook Server is running');
+// Middleware for parsing JSON bodies
+app.use(express.json());
+
+// Webhook endpoint for Strapi
+app.post('/webhook', (req, res) => {
+    console.log('Webhook received:', req.body);
+    
+    // Emit the data to all connected socket clients
+    io.emit('postCreated', req.body);
+
+    // Respond to the webhook
+    res.status(200).send('Webhook received');
 });
 
-app.listen(PORT, () => {
+app.get('/', (req, res) => {
+    res.send('Webhook Server is running!');
+});
+
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
